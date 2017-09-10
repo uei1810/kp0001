1ca41c1cb53bebd0f40eaa380894152d3d3fb50e
Elvira <elvira.utepeshova@gmail.com>
2017-09-10 16:01:06 +0300

    +изменение в cub()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int [] p ; static int [] w ;
-    static int n, c;
-    static int j = 0, zz = 0, z = 0;
-    static int[] x ; static  int[] xx ;
-    static int u = 0;
+    public static int [] p ; public static int [] w ;public static int[] sorted;
+    public static int n, c, c1;
+    public static int j = 0, zz = 0, z = 0;
+    public static int[] x ;public static  int[] xx ;
+    public static int u = 0;
 
     public static void main(String[] args) throws Exception
     {
@@ -17,6 +17,7 @@ public class Main {
         n = Integer.parseInt(reader.readLine());
         System.out.println("Введите вместимость");
         c = Integer.parseInt(reader.readLine());
+        c1=c;
         p = new int[n+1];
         w = new int[n+1];
         x = new int[n];
@@ -29,8 +30,10 @@ public class Main {
             p[i] = Integer.parseInt(reader.readLine());
 
         }
-        w[n] = w[n - 1] + 100;
+        w[n] = w[n - 1] + 10000;
         p[n] = 0;
+
+
 
 
 
@@ -53,30 +56,79 @@ public class Main {
         int m = j - 1;
         int k = j;
         int d = 0;
-        //System.out.println("это текущая вместимость = " + c);
-        while ( sum < c )
+
+        int aa[] = new int[n - j + 1]; int bb[] = new int[n - j + 1];
+        aa[n-j] = c1; bb[n-j] = n;
+        for(int i = 0; i < n - j; i++)
         {
-            m = m + 1;
-            sum = sum + w[m ];
+            aa[i] = w[j + i];
+            bb[i] = j + i;
+            aa[n-j] = aa[n-j]+aa[i];
+            bb[n-j] = bb[n-j]+bb[i];
+        }
+
+        bubble(aa, n - j, bb);
+
+
+        int sum1 = 0; int h1 = 0;
+
+        while ( (sum1 <= c))
+        {
+            sum1 = sum1 + aa[h1];
+            h1 ++;
+
+        }
+        h1--;sum1 = sum1 - aa[h1];
+
+        int sum2 = 0; int h2 = n - j - 1;
+        while ((sum2 <= c) && (h2>=0))
+        {
+            sum2 = sum2 + aa[h2];
+            h2--;
+
+        }
+        if (h2 >=0)
+        {
+            h2 ++; sum2 = sum2 - aa[h2];
+        }
+
+
+        h2 = n - j - 1 - h2;
+
+        if ((h1 == h2) && (h1 != 0) && (h1 + h2 <= n - j))
+        {
+            for (int i = n - j - 1; i >= n - j - h2; i--)
+            {
+                x[bb[i]] = 1;
+                z = z + p[bb[i]];
+            }
+
+            updates1();
+        }
+        else {
+            while (sum < c) {
+                m = m + 1;
+                sum = sum + w[m];
+
+
+            }
+            while (k < m) {
+
+                a = a + p[k];
+                d = d + w[k];
+                k = k + 1;
+            }
+            u = (int) (a + ((c - d) * p[m] / w[m]));
+
+
+            if (zz >= z + u) {
+                backtrack();
+            } else {
+                forward();
+            }
 
 
         }
-        while ( k < m )
-        {
-
-            a = a + p[k];
-            d = d + w[k];
-            k = k + 1;
-        }
-            u = (int)(a + ((c - d)*p[m]/w[m]));
-            //System.out.println(" это верхняя граница " + u);
-            if (zz >= z+u)
-               {   backtrack();}
-            else
-                { forward();}
-
-
-
      }
      public static void forward ()
      {
@@ -86,14 +138,14 @@ public class Main {
              z = z + p[j];
              x[j] = 1;
              j = j +1;
-             //System.out.println("Это j = " + j + " иr x = " + x[j-1]);
+
          }
          if (j <= n - 1)
          {
              x[j] = 0;
-             //System.out.println("это j = " + j + " и х = " + x[j]);
+
              j = j + 1;
-             //System.out.println("это последняя j = " + j);
+
          }
          if (j < n - 1) {cub();}
          if (j == n - 1) {forward();}
@@ -107,7 +159,7 @@ public class Main {
              for (int k = 0; k < n; k++)
              {
                  xx[k] = x[k];
-                 //System.out.print(xx[k] + " ");
+
              }
 
          }
@@ -117,16 +169,16 @@ public class Main {
              c = c + w[n-1];
              z = z - p[n-1];
              x[n-1] = 0;
-             //System.out.println(" это x[6] = " + x[n-1]);
+
 
          }
-         //System.out.println(" это backtrack из updates ");
+
          backtrack();
      }
      public static void backtrack ()
      {   int f = 0;
          int k = j;
-         //System.out.println( "изначальное k " + k);
+
          while ((f == 0) && (k >= 0))
          {
 
@@ -135,7 +187,7 @@ public class Main {
              if (k == -1) break;
              if (x[k] == 1)
                  f = 1;
-             //System.out.println( "новое f " + f);
+
          }
 
          if (f == 0) return;
@@ -143,8 +195,40 @@ public class Main {
          z = z - p[k];
          x[k] = 0;
          j = k + 1;
+
          cub();
 
 
      }
+    public static void bubble(int[] a, int n, int b[])
+    {
+        for (int i=n-1; i>=0; i--)
+        {
+            for (int j=0; j<i; j++)
+            {
+                if (a[j] > a[j+1])
+                {
+                    int tmp = a[j];
+                    int tmp2 = b[j];
+                    a[j] = a[j+1];
+                    b[j] = b[j + 1];
+                    a[j+1] = tmp;
+                    b[j+1] = tmp2;
+                }
+            }
+        }
+    }
+    public static void updates1()
+    {
+
+            zz = z;
+            for (int k = 0; k < n; k++)
+            {
+                xx[k] = x[k];
+                //System.out.print(xx[k] + " ");
+            }
+
+
+
+    }
 }
